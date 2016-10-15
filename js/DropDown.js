29e5c62a880a653bf7df7b4ae39a9a74b91b5e5f
ImDiegoDM <dm.diego.bh@gmail.com>
2016-10-14 21:57:49 -0300

    Second Comit
    adicionado os dropdows e uma imagem de mockup para as coleçoes

--- js/DropDown.js
+++ js/DropDown.js
@@ -1,4 +1,74 @@
-var masc = document.getElementById('masc');
-var fem = document.getElementById('fem');
-var inf = document.getElementById('inf');
-var teen = document.getElementById('ten');
+var masc ;
+var fem ;
+var inf ;
+var teen ;
+var primeiroP;
+
+window.onload = function(){
+  masc = document.getElementById('mascu');
+  fem = document.getElementById('fem');
+  inf = document.getElementById('inf');
+  teen = document.getElementById('ten');
+  primeiroP = document.getElementById('pp');
+}
+
+
+function ShowMasc() {
+  masc.style.display= 'block';
+  fem.style.display= 'none';
+  inf.style.display= 'none';
+  teen.style.display= 'none';
+  primeiroP.style.display= 'none';
+}
+
+function ShowFemi() {
+  masc.style.display= 'none';
+  fem.style.display= 'block';
+  inf.style.display= 'none';
+  teen.style.display= 'none';
+  primeiroP.style.display= 'none';
+}
+
+function ShowInfi() {
+  masc.style.display= 'none';
+  fem.style.display= 'none';
+  inf.style.display= 'block';
+  teen.style.display= 'none';
+  primeiroP.style.display= 'none';
+}
+
+function ShowTeen() {
+  masc.style.display= 'none';
+  fem.style.display= 'none';
+  inf.style.display= 'none';
+  teen.style.display= 'block';
+  primeiroP.style.display= 'none';
+}
+
+function ShowPP() {
+  masc.style.display= 'none';
+  fem.style.display= 'none';
+  inf.style.display= 'none';
+  teen.style.display= 'none';
+  primeiroP.style.display= 'block';
+  console.log(primeiroP);
+}
+
+function HideAll()
+{
+  masc.style.display= 'none';
+  fem.style.display= 'none';
+  inf.style.display= 'none';
+  teen.style.display= 'none';
+  primeiroP.style.display= 'none';
+}
+
+function AddClass(obj,style) {
+  obj.classList.add(style);
+  obj.classList.remove('colorBlack');
+}
+
+function RemoveClass(obj,style) {
+  obj.classList.remove(style);
+  obj.classList.add('colorBlack');
+}
